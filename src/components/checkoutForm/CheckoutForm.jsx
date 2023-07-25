c29498fca4942f59800a0802a57be5a3d44a452e
yuvithakur007 <01yuvisingh@gmail.com>
2023-07-25 23:48:47 +0530

    change

--- src/components/checkoutForm/CheckoutForm.jsx
+++ src/components/checkoutForm/CheckoutForm.jsx
@@ -60,7 +60,7 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:5173/success",
+        return_url: "https://developershub.vercel.app/success",
       },
     });
 
--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://dev-hub-back-end.vercel.app/api/",
   withCredentials: true,
 });
 
